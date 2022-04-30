d6aa151911c4ddf7f98f20982e2a665c12da0a0a
96-Zachary <zijian96@tongji.edu.cn>
2022-04-30 23:08:29 +0800

    Create evaluation.py

---
+++ evaluation.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+def i2t(sims):
+    # sims: Numpy = [n_imgs, n_txts] = [1K, 5K]
+    npts = sims.shape[0]
+    ranks = np.zeros(npts)
+    top1 = np.zeros(npts)
+    for index in range(npts):
+        inds = np.argsort(sims[index])[::-1]
+        # Score
+        rank = 1e20
+        for i in range(5 * index, 5 * index + 5, 1):
+            tmp = np.where(inds == i)[0][0]
+            if tmp < rank:
+                rank = tmp
+        ranks[index] = rank
+        top1[index] = inds[0]
+
+    # Compute metrics
+    r1 = 100.0 * len(np.where(ranks < 1)[0]) / len(ranks)
+    r5 = 100.0 * len(np.where(ranks < 5)[0]) / len(ranks)
+    r10 = 100.0 * len(np.where(ranks < 10)[0]) / len(ranks)
+    return (r1, r5, r10)
+
+def t2i(sims):
+    # sims: Numpy = [n_imgs, n_txts]
+    npts = sims.shape[0]
+    ranks = np.zeros(5 * npts)
+    top1 = np.zeros(5 * npts)
+
+    # --> (5K(texts), K(images))
+    sims = sims.T
+
+    for index in range(npts):
+        for i in range(5):
+            inds = np.argsort(sims[5 * index + i])[::-1]
+            ranks[5 * index + i] = np.where(inds == index)[0][0]
+            top1[5 * index + i] = inds[0]
+
+    # Compute metrics
+    r1 = 100.0 * len(np.where(ranks < 1)[0]) / len(ranks)
+    r5 = 100.0 * len(np.where(ranks < 5)[0]) / len(ranks)
+    r10 = 100.0 * len(np.where(ranks < 10)[0]) / len(ranks)
+    return r1, r5, r10
